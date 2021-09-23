a297fd64552c032ada08cad9f9ca60bb81030b58
Neema93 <89119220+Neema93@users.noreply.github.com>
2021-09-23 10:38:12 -0400

    Add files via upload

---
+++ films.js
@@ -0,0 +1,6 @@
+const director = "Steven Spielberg";
+const films = ["Jaws", "Raiders of the Lost Ark", "A.I."];
+films[2] = "Jurassic Park";
+console.log(films);
+films.push("A.I.");
+console.log(films);
